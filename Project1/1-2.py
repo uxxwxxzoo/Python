b14127ae19a590e3e47e323fc97876ced41c2326
uxxwxxzoo <uxxwxxzoo@users.noreply.github.com>
2020-08-13 15:45:15 +0900

    Create 1-2.py
    숫자 포매팅 기능 사용하기

---
+++ Project1/1-2.py
@@ -0,0 +1,11 @@
+n1 = 35.6354
+n2 = 82.8192
+n3 = 3
+
+print("n1 = {0} \t n2 = {1} \t n3 = {2}".format(n1,n2,n3))
+print("")
+print("")
+print("n1 + n2 = {0:10.2f}\t n1 - n2 = {1:10.2f}".format(n1+n2, n1-n2))
+print("n1 * n2 = {0:10.2f}\t n1 / n2 = {1:10.2f}".format(n1*n2, n1/n2))
+print("n1 ** n3 = {0:10.2f}\t n1 % n2 = {1:10.2f}".format(n1**n3, n1%n2))
+print("n2 // n1 = {0:10.2f}".format(n2//n1))
